a3dee18c1dd7bcaa398d68d95e363d59097002e3
郑昊 <zhenghao@d207-023-170-047.wireless.sfu.ca>
2016-11-04 17:33:50 -0700

    layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,24 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
 
-  get 'static_pages/help'
+	root 'static_pages#home'
+  get 'users/index'
 
+  get 'users/show'
+
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'users/edit'
+
+  get 'users/update'
+
+  get 'users/destroy'
+
+  #get 'static_pages/home', to: 'static_pages#home'
+
+  get '/help', to: 'static_pages#help'
+
+  get '/about', to: 'static_pages#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
